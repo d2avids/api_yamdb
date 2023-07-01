e973a1c69b3a48d3640a6ae25f72fad72aa7550f
Игорь Плетенный <pli97@yandex.ru>
2023-07-01 19:49:37 +0300

    Review and Comment ser/view

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from reviews.models import CustomUser
+from reviews.models import CustomUser, Review, Comment, Title
+from rest_framework.generics import get_object_or_404
+from rest_framework.exceptions import ValidationError
 
 
 class CustomTokenObtainSerializer(TokenObtainSerializer):
@@ -21,3 +23,46 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ("username", "email", "first_name", "last_name", "bio", "role")
+
+class ReviewSerializer(serializers.ModelSerializer):
+    '''Сериализатордля для модели Review'''
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы не можете добавить более'
+                                      'одного отзыва на произведение')
+        return data
+
+    class Meta:
+        model = Review
+        fields = ('id', 'author', 'text', 'score', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Comment'''
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
-from reviews.models import CustomUser
+from reviews.models import CustomUser, Review, Title
+from django.shortcuts import get_object_or_404
+from .permissions import IsAdminOrReadOnly, IsModerator
 
-from .serializers import CustomTokenObtainSerializer, CustomUserSerializer
+from .serializers import CustomTokenObtainSerializer, CustomUserSerializer, ReviewSerializer, CommentSerializer
 
 
 class TokenObtainView(TokenObtainPairView):
@@ -12,3 +14,34 @@ class TokenObtainView(TokenObtainPairView):
 class CustomUserModelViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAdminOrReadOnly, IsModerator]
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminOrReadOnly, IsModerator]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        serializer.save(author=self.request.user, review=review)
+        
