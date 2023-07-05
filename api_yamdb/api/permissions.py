333d446c5745847e6018c8e3d34aaf61dd98f1b6
David Saidov <delightxxls@gmail.com>
2023-07-05 12:15:08 +0300

    fix permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,13 +9,12 @@ class IsAuthorModeratorAdmin(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        def has_object_permission(self, request, view, obj):
-            return (
-                    request.method in permissions.SAFE_METHODS
-                    or obj.author == request.user
-                    or request.user.is_moderator
-                    or request.user.is_admin
-            )
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
+        )
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
