ce894f6a4524994910bd37d6f23d7c5dec504d5c
Игорь Плетенный <pli97@yandex.ru>
2023-07-08 08:51:54 +0300

    after_review_improve

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -14,14 +14,14 @@ from .views import (
 
 router_v1 = DefaultRouter()
 router_v1.register("users", CustomUserModelViewSet, basename="user")
-router_v1.register(r"titles", TitleViewSet, basename="title")
-router_v1.register(r"genres", GenreViewSet, basename="genre")
-router_v1.register(r"categories", CategoryViewSet, basename="category")
+router_v1.register("titles", TitleViewSet, basename="title")
+router_v1.register("genres", GenreViewSet, basename="genre")
+router_v1.register("categories", CategoryViewSet, basename="category")
 router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="reviews"
 )
 router_v1.register(
-    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)" r"/comments",
+    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     CommentViewSet,
     basename="comments",
 )
